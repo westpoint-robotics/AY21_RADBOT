e429e9c8da590f0a3b0f4449ff45fe91858371e7
e-shepherd-6 <ethan.shepherd@westpoint.edu>
2020-04-14 11:55:43 -0400

    Cleanup

--- radgenerator.py
+++
@@ -1,9 +0,0 @@
-import random
-
-def rad_gen():
-    while True:
-        num = random.randrange(0,101)
-        num1 = random.randrange(0,101)
-        num2 = random.randrange(0,101)
-        num3 = random.randrange(0,101)
-        yield (num, num1, num2, num3)
